830153820a992d9f42124a3367db1008e3d981c8
Lucas de Sousa <66752018+lucas-fsousa@users.noreply.github.com>
2021-05-29 21:39:24 -0300

    Add files via upload

---
+++ ModeloDatabase/ModeloDatabase_MS_Sql.sql
@@ -0,0 +1,28 @@
+USE [Banco_Teste]
+GO
+
+/****** Object:  Table [dbo].[cadastro]    Script Date: 29/05/2021 21:38:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[cadastro](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[login] [varchar](20) NOT NULL,
+	[senha] [varchar](60) NOT NULL,
+	[data_creat_senha] [datetime] NOT NULL,
+	[email] [varchar](60) NOT NULL,
+	[auto_pass] [varchar](5) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[login] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
